b809657d670c21638094e73ebfd2ab3bb13dfef4
Mizsakptr <mizsakptr@gmail.com>
2023-09-17 03:37:04 +0200

    Added forwards and backwards buttons

--- src/main/java/com/example/pinned/App.java
+++ src/main/java/com/example/pinned/App.java
@@ -27,7 +27,4 @@ public class App extends Application {
         launch();
     }
 
-
-
-
 }
--- src/main/java/com/example/pinned/Window.java
+++ src/main/java/com/example/pinned/Window.java
@@ -10,17 +10,29 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Window extends Stage {
     public Window() {
+        ArrayList<String> history = new ArrayList<>();
+        AtomicInteger current = new AtomicInteger();
+        current.set(0);
+        AtomicBoolean backorforw = new AtomicBoolean(false);
         TextField text = new TextField();
-        text.setPrefWidth(700);
+        text.setText("https://www.google.com/");
+        text.setPrefWidth(600);
+        history.add(text.getText());
         Button go = new Button("Go");
         Button newtab = new Button("+");
+        Button backwards = new Button("<-");
+        Button forwards = new Button("->");
 
         WebView webView = new WebView();
         WebEngine webEngine = webView.getEngine();
 
-        webEngine.load("https://www.google.com/");
+        webEngine.load(text.getText());
 
         go.setOnAction(event -> {
 
@@ -29,15 +41,59 @@ public class Window extends Stage {
         });
 
         text.setOnKeyPressed(ke -> {
+
             if (ke.getCode().equals(KeyCode.ENTER)) {
+                
                 webEngine.load(text.getText());
+                
             }
+
         });
+
+        webEngine.locationProperty().addListener((observable, oldURL, newURL) -> {
+
+            System.out.println("Új URL: " + newURL);
+            text.setText(newURL);
+            
+            if(!backorforw.get()){
+                current.getAndIncrement();
+                history.add(newURL);
+            }
+
+            backorforw.set(false);
+            System.out.printf(history.toString());
+            System.out.printf(String.valueOf(current.get()));
+            
+        });
+
+        backwards.setOnAction(event ->{
+
+            backorforw.set(true);
+            current.getAndDecrement();
+            webEngine.load(history.get(current.get()));
+            System.out.printf(history.toString());
+            System.out.printf(String.valueOf(current.get()));
+
+        });
+
+        forwards.setOnAction(event ->{
+
+            backorforw.set(true);
+            current.getAndIncrement();
+            webEngine.load(history.get(current.get()));
+            System.out.printf(history.toString());
+            System.out.printf(String.valueOf(current.get()));
+
+        });
+
+
+
+
 
         newtab.setOnAction(e -> App.newWindow());
 
         HBox inputBox = new HBox(10);
-        inputBox.getChildren().addAll(text, go, newtab);
+        inputBox.getChildren().addAll(backwards,forwards,text, go, newtab);
 
         BorderPane root = new BorderPane();
         root.setTop(inputBox);
